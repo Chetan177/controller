750bde2a6d8b7852501a1d0d28bf48923031497f
chetan177 <chetanpandey177@gmail.com>
2023-05-02 00:22:51 +0530

    Namespace constraint added for the controller

--- controller/cmd/main.go
+++ controller/cmd/main.go
@@ -72,6 +72,7 @@ func main() {
 		LeaderElection:          enableLeaderElection,
 		LeaderElectionID:        "b4652c81.controller.demo",
 		LeaderElectionNamespace: "demo", // Leader election namespace
+		Namespace:               "demo",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
